58129952254153478048701d62fb9f47059ed593
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 07:17:08 +0300

    Completed exercises 01, 02 with all tests passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, repeats) {
+    let word_repeated = '';
 
+    if (repeats < 0) return 'ERROR';
+    
+    for (let i = 0; i < repeats; i++) {
+        word_repeated += word;
+    }
+    return word_repeated;
 };
 
 // Do not edit below this line
