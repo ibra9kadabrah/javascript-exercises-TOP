40a8c49c434a538756814cb649202cf8b837424e
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 07:56:40 +0300

    solved first test (remove one element) in 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arr_remove) {
+    let new_arr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == arr_remove) {
+            new_arr = arr.toSpliced(i, 1);
+        }
+    }
+    return new_arr
 };
 
 // Do not edit below this line
