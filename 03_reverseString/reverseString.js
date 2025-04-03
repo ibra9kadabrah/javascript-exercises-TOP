10f5130ebcef278898a4da23afe491052ad7be76
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 07:43:38 +0300

    passed all test in 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let length = word.length;
+    let reversed_string = '';
+    
+    for (let i = length - 1; i >= 0; i--) {
+        reversed_string += word.at(i);
+    }
+    return reversed_string;
 };
 
 // Do not edit below this line
