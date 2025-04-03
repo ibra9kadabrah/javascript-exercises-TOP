e1a012645da27e469464dbc8d13ed22b3654f20e
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 11:05:10 +0300

    solved all test for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(feh_num) {
+    let cels = (feh_num - 32) * 5/9;
+    return Math.round(cels * 10) / 10;
+  };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let feh = (cels * 9/5) + 32;
+  return Math.round(feh * 10) / 10;
 };
 
 // Do not edit below this line
