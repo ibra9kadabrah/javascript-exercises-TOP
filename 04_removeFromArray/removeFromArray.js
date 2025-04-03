f02df147356890d9d79c350dfefabd52ecfa05a4
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 08:09:16 +0300

    passed all 04 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,17 @@
-const removeFromArray = function(arr, arr_remove) {
-    let new_arr = [];
+"use strict"
+
+const removeFromArray = function(arr, ...arr_remove) {
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == arr_remove) {
-            new_arr = arr.toSpliced(i, 1);
+        for (let j = 0; j < arr_remove.length; j++) {
+            if (arr[i] === arr_remove[j]) {
+                arr= arr.toSpliced(i, 1);
+                i = 0;
+            }
         }
     }
-    return new_arr
+    return arr
 };
 
+removeFromArray([1,2,3,4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
