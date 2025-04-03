1a17e5397fb692e74c2efe081c5082aac8f63660
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 07:47:42 +0300

    refined code to use string and array methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(word) {
-    let length = word.length;
-    let reversed_string = '';
-    
-    for (let i = length - 1; i >= 0; i--) {
-        reversed_string += word.at(i);
-    }
-    return reversed_string;
+    return word.split('').reverse().join('');
 };
 
 // Do not edit below this line
