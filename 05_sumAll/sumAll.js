c428a8e4f55fd8d9b3f807d49e5768a93b9ee018
ibrahim bahlawan <ibrabahlawan9@gmail.com>
2025-04-03 08:25:15 +0300

    solved 05. checking for integer solves all potentail problems (string number, different type etc)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(n,z) {
+    let sum = 0;
+    if (!(Number.isInteger(n) && Number.isInteger(z)) || (n < 0 || z < 0)) return "ERROR"; 
+    if (n < z) {
+        for (; n <= z; n++) sum += n;
+    } else {
+        for (; n >= z; n--) sum += n;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
